deff71ffe008222d29bf47e81fd2ffa758771c63
frog <frog@frogbox.se>
2025-06-24 09:58:00 +0200

    Fixed bug causing poems to overlap when spamming the button.

--- src/script.js
+++ src/script.js
@@ -1,7 +1,12 @@
 const starting_lines = ["The clouds were parting", "The desktop was clean", "Windows is buggy", "Linux is cooler", "Arch is for the cool", "Programming is fun", "Hardware is real tough"]
 const middle_lines = ["Now is always important", "Calculators are cooler", "It holds a place in my heart", "Flipper zeros remain great", "Pen and paper is pretty"]
 const ending_lines = ["The computer exploded", "Another day passed", "Nothing will change today", "Will that ever change", "Tides are never still", "F*ck you NVIDIA" ]
+
+let text = "";
+let index = 0;
+
 function Generate() {
+    index = 0;
     text = selectFromArray(starting_lines) + "\n" + selectFromArray(middle_lines) + "\n" + selectFromArray(ending_lines);
     text = text.replaceAll(" ", "\u2008")
     WriteSlowly(text)
@@ -10,14 +15,15 @@ function Generate() {
 
 
 
-function WriteSlowly(text) {
+function WriteSlowly() {
     document.getElementById("textbox").innerText = "";
     document.getElementById("textbox").style = "color:black;"
     WriteFirstLetter(text, 0)
 }
-function WriteFirstLetter(text, index) {
+function WriteFirstLetter() {
     document.getElementById("textbox").innerText += text.substring(index, 1 + index);
     console.log(text)
-    setTimeout(() => WriteFirstLetter(text, index + 1), 90)
+    index += 1;
+    setTimeout(() => WriteFirstLetter(), 90)
 }
 Generate()
